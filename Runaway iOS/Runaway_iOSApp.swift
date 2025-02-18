ae410c6c6465661bb788adb7f075603e233ee4bd
Jack Rudelic <jackrudelic@gmail.com>
2025-02-18 08:22:18 -0600

    Initial Commit

---
+++ Runaway iOS/Runaway_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Runaway_iOSApp.swift
+//  Runaway iOS
+//
+//  Created by Jack Rudelic on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Runaway_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Runaway iOSTests/Runaway_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Runaway_iOSTests.swift
+//  Runaway iOSTests
+//
+//  Created by Jack Rudelic on 2/18/25.
+//
+
+import Testing
+@testable import Runaway_iOS
+
+struct Runaway_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
